3d3ef19c92c1e7de56a40fdb394aea2a7e39f3d4
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-07-23 23:10:05 -0500

    Add API project

---
+++ ApiPlayer/ApiPlayer/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPlayer.Models
+{
+    public class Category
+    {
+        [Key]
+        public int IdCategory { get; set; }
+        public string NameCategory { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
